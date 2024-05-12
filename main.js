4bf1af75892af3ae1b7785082f2523403d6134eb
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-05-12 14:53:59 +0100

    bind

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'A warm fuzzy pair of socks.'
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3'
         }
     }
 })
