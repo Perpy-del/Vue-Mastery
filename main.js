2d0e42241804daeb5e77ccc4ddfe2f8c8663fd6f
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-05-12 17:39:00 +0100

    passing props up

--- main.js
+++ main.js
@@ -1,54 +1,19 @@
 const app = Vue.createApp({
   data() {
     return {
-      cart: 0,
-      product: 'Socks',
-      brand: 'Vue Mastery',
-      onSale: false,
-      selectedVariant: 0,
-      details: ['50% cotton', '30% wool', '20% polyester'],
-      variants: [
-        {
-          id: 2234,
-          color: 'green',
-          image: './assets/images/socks_green.jpg',
-          quantity: 50,
-        },
-        {
-          id: 2235,
-          color: 'blue',
-          image: './assets/images/socks_blue.jpg',
-          quantity: 0,
-        },
-      ],
+      cart: [],
+      premium: false,
     };
   },
   methods: {
-    addToCart() {
-      this.cart += 1;
+    updateCart(id) {
+        this.cart.push(id);
     },
-    removeFromCart() {
-      this.cart -= 1;
-    },
-    updateVariant(index) {
-      this.selectedVariant = index;
-    },
-  },
-  computed: {
-    title() {
-      return this.brand + ' ' + this.product;
-    },
-    image() {
-      return this.variants[this.selectedVariant].image;
-    },
-    inStock() {
-      return this.variants[this.selectedVariant].quantity;
-    },
-    brandOnSale() {
-      if (this.onSale) {
-        return this.brand + ' ' + this.product + ' is on sale.';
-      }
-      return ''
-    },
+    removeFromCart(id) {
+        const index = this.cart.indexOf(id);
+        if (index > -1) {
+            this.cart.splice(index, 1);
+        }
+    }
   },
 });
