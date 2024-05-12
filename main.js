6ab68b1953889c12586145ec0774318cdabdc4f1
Perpetual Meninwa <perpetualmeninwa@gmail.com>
2024-05-12 16:43:04 +0100

    computed values

--- main.js
+++ main.js
@@ -3,12 +3,23 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: 'Socks',
-      image: './assets/images/socks_blue.jpg',
-      inStock: false,
+      brand: 'Vue Mastery',
+      onSale: false,
+      selectedVariant: 0,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+        {
+          id: 2234,
+          color: 'green',
+          image: './assets/images/socks_green.jpg',
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: 'blue',
+          image: './assets/images/socks_blue.jpg',
+          quantity: 0,
+        },
       ],
     };
   },
@@ -19,8 +30,25 @@ const app = Vue.createApp({
     removeFromCart() {
       this.cart -= 1;
     },
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    brandOnSale() {
+      if (this.onSale) {
+        return this.brand + ' ' + this.product + ' is on sale.';
+      }
+      return ''
     },
   },
 });
